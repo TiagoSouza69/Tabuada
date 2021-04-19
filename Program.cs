48622cfe210d634d85c2124404065a2de2e1790b
TiagoSouza69 <tiagoblackusmoon@gmail.com>
2021-04-19 11:50:08 -0300

    tabuada

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int i, r, con, n;
+	         double d;	
+             
+
+            Console.WriteLine("Feito por Tiago gostosão!");
+		    Console.WriteLine("Escreva o número ao qual você quer fazer as operações:");
+            n = int.Parse(Console.ReadLine());
+            con = n ;
+
+            for (i=0;i<=10;i++) {
+            r= con + i;
+			Console.WriteLine(+con +" + " + i +"= " +r);
+			
+		}
+        Console.WriteLine("_________________________");
+		for (i=0;i<=10;i++) {
+			r= con - i;
+			Console.WriteLine(+con +" - " + i +"= " +r);
+			
+		}
+        Console.WriteLine("_________________________");
+		for (i=0;i<=10;i++) {
+			r= con * i;
+			Console.WriteLine(+con +" x " + i +"= " +r);
+			
+		}
+        Console.WriteLine("_________________________");
+		for (i=1;i<=10;i++) {
+			d = con / i;
+			Console.WriteLine(+con +" : " + i +"= " +d);
+			
+		}
+        }
+    }
+}
